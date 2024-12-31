283c71c0b0ad4f76dce2440512706d23ce165841
Wolfgang Mattuttis <wolfgang@mattuttis.com>
2024-12-31 21:37:22 +0100

    Added gorilla mux; return members JSON encoded

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mattuttis/zoekdeware-be
 
 go 1.23.3
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	birthDate, _ := time.Parse("2006-01-02", "1976-02-07")
+	router := mux.NewRouter()
+	router.HandleFunc("/members", func(w http.ResponseWriter, r *http.Request) {
+		var members = []Member{
+			{Id: "1", FirstName: "John", LastName: "Doe", BirthDate: birthDate},
+			{Id: "2", FirstName: "Jane", LastName: "Doe", BirthDate: birthDate},
+			{Id: "3", FirstName: "John", LastName: "Smith", BirthDate: birthDate},
+		}
+		if err := json.NewEncoder(w).Encode(members); err != nil {
+			panic(err)
+		}
+	})
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+type Member struct {
+	Id        string    `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	BirthDate time.Time `json:"birth_date"`
 }
